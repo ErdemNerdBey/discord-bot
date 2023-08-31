407fb5fc0cc30b082317f642402919c27bf206df
ErdemNerdBey <89967771+ErdemNerdBey@users.noreply.github.com>
2023-08-31 12:14:55 +0200

    Add files via upload

--- gamePicker.py
+++ gamePicker.py
@@ -1,17 +1,6 @@
 import discord
 from discord import Intents
 
-games = {
-    "Fortnite": 0,
-    "Call of Duty": 0,
-    "Fifa 23": 0,
-    "CS:GO": 0,
-    "BTD 6": 0,
-    "Minecraft": 0,
-    "Valorant": 0,
-    "Roblox": 0,
-}
-
 intents = Intents.default()
 intents.message_content = True
 intents.guilds = True
@@ -40,6 +29,39 @@ async def on_ready():
     print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
 
 
+nummers = {
+    1: '\u0031\u20E3',  # :one:
+    2: '\u0032\u20E3',  # :two:
+    3: '\u0033\u20E3',  # :three:
+    4: '\u0034\u20E3',  # :four:
+    5: '\u0035\u20E3',  # :five:
+    6: '\u0036\u20E3',  # :six:
+    7: '\u0037\u20E3',  # :seven:
+    8: '\u0038\u20E3',  # :eight:
+    9: '\u0039\u20E3',  # :nine:
+    10: '\uD83D\uDD1F'  # :keycap_ten:
+}
+
+games = {
+    "Fortnite": 0,
+    "Call of Duty": 0,
+    "Fifa 23": 0,
+    "CS:GO": 0,
+    "BTD 6": 0,
+    "Minecraft": 0,
+    "Valorant": 0,
+    "Roblox": 0,
+}
+
+
+async def on_reaction_add(reaction, user):
+    if user != bot.user:  # Check if the reaction is not from the bot itself
+        emoji = reaction.emoji
+        if emoji in emoji_to_number:
+            number = emoji_to_number[emoji]
+            await reaction.message.channel.send(f'You chose number {number}')
+
+
 async def favo_game(message: discord.Message):
     await message.channel.send("Kies 3 favoriete spellen")
     count = 1
@@ -50,7 +72,9 @@ async def favo_game(message: discord.Message):
 
     embedVar = discord.Embed(title="Games")
     embedVar.add_field(name="", value=bericht)
-    await message.channel.send(embed=embedVar)
+    sent_message = await message.channel.send(embed=embedVar)
+    for i in range(len(games)):
+        await sent_message.add_reaction(f'{nummers[i + 1]}')
     # await message.channel.send(bericht)
 
 
@@ -63,6 +87,4 @@ async def on_message(message: discord.Message):
 
 
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
-bot.run("MTE0NjAyMzIxNTY3Njc4ODc5Ng.Gxi2ek.0fFiOps-27E77Jboz7kZqeYzBAjiQfHEtW1bQ4")
-
-#mijn bot toke: MTE0NjAyMTc4NTUyODE3MjY1NA.GlCfVc.20Xl1uoAV36b9CQa0X-E8aCticpj1xH1U5CnIo
+bot.run("MTE0NjAyMTc4NTUyODE3MjY1NA.GHVCni.zL8sSgJq9ZQLjla2o340UGJWMjt39SZTVNLY_0")
